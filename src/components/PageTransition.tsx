81ea1f17f2c2954b944319c6b2b6ff821a40e4f3
Cookie <taruta1110@yahoo.co.jp>
2021-07-19 05:04:45 +0900

    Update PageTransition

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -1,11 +1,26 @@
 import { useCheck } from 'hooks';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { setActiveTime } from 'utils/common';
+import { useRouter } from 'utils/next';
 import styles from '../assets/scss/components/PageTransition.module.scss';
 
 export default function PageTransition() {
+  const router = useRouter();
   const isMounted = useCheck();
+  const [isTransition, _isTransition] = useState<boolean>(false);
+  useEffect(() => {
+    if (localStorage.getItem('path') !== router.pathname) setActiveTime(_isTransition, 1200);
+    localStorage.setItem('path', router.pathname);
+  }, [router.pathname]);
   return (
     <>
-      <div className={`${styles.entire} ${isMounted && styles.mounted}`}>
+      <div className={`
+        ${styles.entire} 
+        ${isMounted && styles.mounted} 
+        ${isTransition && styles.transitioned} 
+        ${router.pathname == '/' && styles.home}`}
+      >
         <div className={styles.cover}>
           <span>LeMoN</span>
         </div>
