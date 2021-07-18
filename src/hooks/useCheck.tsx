a8e288e1596673191d313242f6ed9056df954c71
Cookie <taruta1110@yahoo.co.jp>
2021-07-19 05:04:59 +0900

    update useCheck

--- src/hooks/useCheck.tsx
+++ src/hooks/useCheck.tsx
@@ -1,12 +1,15 @@
 import { useState, useEffect } from 'react';
+import { useRouter } from 'utils/next';
+
 const useCheck = function () {
   const [isMounted, _isMounted] = useState<boolean>(false);
+  const router = useRouter();
   useEffect(() => {
     _isMounted(true);
     return () => {
       _isMounted(false);
     };
-  }, [isMounted]);
+  }, [isMounted, router.pathname]);
   return isMounted;
 };
 export default useCheck;
