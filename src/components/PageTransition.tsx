bb9a36f4b9517c512f1d88247b11c5995d8d8f24
Cookie <taruta1110@yahoo.co.jp>
2021-07-24 22:29:50 +0900

    page transition update

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -1,24 +1,13 @@
-import { useMount } from 'hooks';
-import { useState } from 'react';
-import { useEffect } from 'react';
-import { setActiveTime } from 'utils/common';
 import { useRouter } from 'utils/next';
 import styles from '../assets/scss/components/PageTransition.module.scss';
 
 export default function PageTransition() {
   const router = useRouter();
-  const isMounted = useMount();
-  const [isTransition, _isTransition] = useState<boolean>(false);
-  useEffect(() => {
-    if (localStorage.getItem('path') !== router.pathname) setActiveTime(_isTransition, 1200);
-    localStorage.setItem('path', router.pathname);
-  }, [router.pathname]);
   return (
     <>
-      <div className={`
+      <div
+        className={`
         ${styles.entire} 
-        ${isMounted && styles.mounted} 
-        ${isTransition && styles.transitioned} 
         ${router.pathname == '/' && styles.home}`}
       >
         <div className={styles.cover}>
