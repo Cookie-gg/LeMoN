68289b9652f40fcc7f3b0c05135d1f74829771db
Cookie <taruta1110@yahoo.co.jp>
2021-07-24 22:30:17 +0900

    update useAgent

--- src/hooks/useAgent.tsx
+++ src/hooks/useAgent.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 const useAgent = () => {
   const [isMobile, _isMobile] = useState<boolean>(false);
   useEffect(() => {
-    if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
+    if (navigator.userAgent.match(/iPhone|Android.+Mobile/) || window.innerWidth < 820) {
       _isMobile(true);
     }
   }, []);
