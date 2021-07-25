3ae64896a11621b2c77ebdc2a8917ef0dc01616f
Cookie <taruta1110@yahoo.co.jp>
2021-07-26 07:59:29 +0900

    add useFirst

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,15 +1,25 @@
-import { useMount } from 'hooks';
-import { ReactNode } from 'react';
+import { useFirst, useMount } from 'hooks';
 import { PageTransition } from 'components';
 import styles from '../assets/scss/components/Frame.module.scss';
+import { ReactElement, cloneElement, Children } from 'react';
+import { useRouter } from 'utils/next';
 
-export default function Frame({ children }: { children: ReactNode }) {
+export default function Frame({ children }: { children: ReactElement }) {
   const isMounted = useMount();
+  const isFirst = useFirst();
+  const router = useRouter();
+  const child = Children.map(children, () => {
+    if (router.pathname === '/') {
+      return cloneElement(children, { isFirst });
+    } else {
+      return children;
+    }
+  });
   return (
     <>
       <main className={`${styles.main} ${isMounted && styles.mounted}`}>
-        <PageTransition />
-        {children}
+        <PageTransition isFirst={isFirst} />
+        {child}
       </main>
     </>
   );
