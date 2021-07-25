818f87ba984b7746c53c4186b26826095bd9a28d
Cookie <taruta1110@yahoo.co.jp>
2021-07-26 07:58:33 +0900

    Update components/page transition

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -2,13 +2,15 @@ import { useMount } from 'hooks';
 import { useRouter } from 'utils/next';
 import styles from '../assets/scss/components/PageTransition.module.scss';
 
-export default function PageTransition() {
+export default function PageTransition({ isFirst }: { isFirst: boolean }) {
   const router = useRouter();
   const isMounted = useMount();
   return (
     <>
       <div
-        className={`${styles.entire} ${isMounted && styles.mounted} ${router.pathname == '/' && styles.home}`}
+        className={`${styles.entire} ${isMounted && styles.mounted} ${
+          router.pathname == '/' && styles.home
+        } ${isFirst && styles.first}`}
       >
         <div className={styles.cover}>
           <span>LeMoN</span>
