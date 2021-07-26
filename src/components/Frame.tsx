10f86876f9630256d89f093fa87d087d6eeaa43c
Cookie <taruta1110@yahoo.co.jp>
2021-07-26 14:20:23 +0900

    Update components/Frame

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,13 +1,21 @@
-import { useFirst, useMount } from 'hooks';
+import { useFirst } from 'hooks';
 import { PageTransition } from 'components';
 import styles from '../assets/scss/components/Frame.module.scss';
 import { ReactElement, cloneElement, Children } from 'react';
 import { useRouter } from 'utils/next';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-export default function Frame({ children }: { children: ReactElement }) {
-  const isMounted = useMount();
+export default function Frame({
+  children,
+  isMounted,
+}: {
+  children: ReactElement;
+  isMounted: boolean;
+}) {
   const isFirst = useFirst();
   const router = useRouter();
+  const [prevChild, _prevChild] = useState<ReactElement>();
   const child = Children.map(children, () => {
     if (router.pathname === '/') {
       return cloneElement(children, { isFirst });
@@ -15,11 +23,14 @@ export default function Frame({ children }: { children: ReactElement }) {
       return children;
     }
   });
+  useEffect(() => {
+    _prevChild(children);
+  }, [router.pathname, children]);
   return (
     <>
       <main className={`${styles.main} ${isMounted && styles.mounted}`}>
         <PageTransition isFirst={isFirst} />
-        {child}
+        {prevChild === children ? child : 'Loading'}
       </main>
     </>
   );
