86be853a29d20377a3b004b5006770bf07922bd2
Cookie <taruta1110@yahoo.co.jp>
2021-07-26 22:58:41 +0900

    add scroll frame

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,18 +1,25 @@
-import { useFirst } from 'hooks';
+import { useFirst, useNode } from 'hooks';
 import { PageTransition } from 'components';
 import styles from '../assets/scss/components/Frame.module.scss';
-import { ReactElement, cloneElement, Children } from 'react';
+import { ReactElement, cloneElement, Children, useCallback } from 'react';
 import { useRouter } from 'utils/next';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function Frame({
   children,
   isMounted,
+  type,
 }: {
   children: ReactElement;
-  isMounted: boolean;
+  isMounted?: boolean;
+  type: string;
 }) {
   const isFirst = useFirst();
   const router = useRouter();
+  const [contents, target] = useNode();
+  const [prevPlace, _prevPlace] = useState<number>(0);
+  const [isDown, _isDown] = useState<boolean>(true);
   const child = Children.map(children, () => {
     if (router.pathname === '/') {
       return cloneElement(children, { isFirst });
@@ -20,14 +27,30 @@ export default function Frame({
       return children;
     }
   });
-  return (
-    <>
-      <main
-        className={`${styles.main} ${isMounted && styles.mounted}`}
+  if (type === 'base') {
+    return (
+      <>
+        <main className={`${styles.main} ${isMounted && styles.mounted}`}>
+          <PageTransition isFirst={isFirst} />
+          {child}
+        </main>
+      </>
+    );
+  } else {
+    return (
+      <div
+        className={styles.scroller}
+        onScroll={() => {
+          console.log(contents!.getBoundingClientRect().height);
+          console.log(contents!.getBoundingClientRect().bottom);
+          
+          _prevPlace(contents!.getBoundingClientRect().bottom);
+        }}
       >
-        <PageTransition isFirst={isFirst} />
-        {child}
-      </main>
-    </>
-  );
+        <div ref={target} className={styles.contents}>
+          {children}
+        </div>
+      </div>
+    );
+  }
 }
