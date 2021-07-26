6a35b34acf2ea8c474c59e5e764728bba7f285e2
Cookie <taruta1110@yahoo.co.jp>
2021-07-26 22:59:21 +0900

    Update pages/about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,9 @@
+import pages from '../assets/scss/pages/About.module.scss';
+import { Frame } from 'components';
 export default function About() {
   return (
-    <>
+    <Frame type="scroll">
       <h1>About Page</h1>
-    </>
+    </Frame>
   );
 }
