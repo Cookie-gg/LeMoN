0c5deceedc07741ee79533a6a614688580a40bbf
Cookie <taruta1110@yahoo.co.jp>
2021-07-28 03:46:24 +0900

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,24 @@
 import pages from '../assets/scss/pages/About.module.scss';
 import { Frame } from 'components';
 import { useState } from 'react';
+import { Image } from 'utils/next';
+import profile from '../assets/img/lemon_gray.png';
+
 export default function About() {
   const [section, _section] = useState<number>(1);
-  console.log(section);
   return (
-    <Frame type="scroll" number={2} section={section} _section={(n: number) => _section(n)}>
+    <Frame type="scroll" section={section} _section={(n: number) => _section(n)}>
       <>
-        <h1>About Page</h1>
+        {
+          <>
+            <p>dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
+          </>
+        }
+        {
+          <>
+            <p>dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
+          </>
+        }
       </>
     </Frame>
   );
