04a5047723eda98fd17574a4181a3930530bb737
Cookie <taruta1110@yahoo.co.jp>
2021-07-27 23:31:58 +0900

    add Frame for scroll

--- src/components/Frame.tsx
+++ src/components/Frame.tsx
@@ -1,33 +1,37 @@
-import { useFirst, useNode } from 'hooks';
+import { useFirst } from 'hooks';
+import { useRouter } from 'utils/next';
 import { PageTransition } from 'components';
 import styles from '../assets/scss/components/Frame.module.scss';
-import { ReactElement, cloneElement, Children, useCallback } from 'react';
-import { useRouter } from 'utils/next';
+import { ReactElement, cloneElement, Children } from 'react';
+import { UIEvent } from 'react';
 import { useState } from 'react';
-import { useEffect } from 'react';
 
 export default function Frame({
   children,
   isMounted,
   type,
+  number = 0,
+  section,
+  _section,
 }: {
   children: ReactElement;
   isMounted?: boolean;
   type: string;
+  number?: number;
+  section?: number;
+  _section?: (n: number) => void;
 }) {
   const isFirst = useFirst();
   const router = useRouter();
-  const [contents, target] = useNode();
-  const [prevPlace, _prevPlace] = useState<number>(0);
-  const [isDown, _isDown] = useState<boolean>(true);
-  const child = Children.map(children, () => {
-    if (router.pathname === '/') {
-      return cloneElement(children, { isFirst });
-    } else {
-      return children;
-    }
-  });
+  const [marginTop, _marginTop] = useState<number>(0);
   if (type === 'base') {
+    const child = Children.map(children, () => {
+      if (router.pathname === '/') {
+        return cloneElement(children, { isFirst });
+      } else {
+        return children;
+      }
+    });
     return (
       <>
         <main className={`${styles.main} ${isMounted && styles.mounted}`}>
@@ -37,20 +41,49 @@ export default function Frame({
       </>
     );
   } else {
-    return (
-      <div
-        className={styles.scroller}
-        onScroll={() => {
-          console.log(contents!.getBoundingClientRect().height);
-          console.log(contents!.getBoundingClientRect().bottom);
-          
-          _prevPlace(contents!.getBoundingClientRect().bottom);
-        }}
-      >
-        <div ref={target} className={styles.contents}>
-          {children}
+    const scrollEvent = (e: UIEvent<HTMLDivElement, globalThis.UIEvent>) => {
+      if (_section) {
+        const el = e.target as HTMLDivElement;
+        const diff = Math.floor((el.scrollTop / (el.scrollHeight - el.clientHeight)) * number);
+        _section(diff !== number ? diff + 1 : number);
+        _marginTop(el.scrollTop);
+      }
+    };
+    return number > 1 ? (
+      <>
+        <div className={styles.scroller} onScroll={(e) => scrollEvent(e)}>
+          <div className={styles.contents} style={{ marginTop: `${marginTop}px` }}>
+            {children}
+          </div>
+          <div className={styles.scroller_height} style={{ height: `${number * 60}%` }}></div>
         </div>
-      </div>
+        <div className={styles.wrapper}>
+          {(() => {
+            const item = [];
+            for (let i = 1; i < number + 1; i++)
+              item.push(
+                <button
+                  key={i}
+                  className={`${i === section && styles.current}`}
+                  style={{ height: `calc(50vh / ${number})` }}
+                  onClick={(e) => {
+                    const el = (e.target as HTMLDivElement).parentElement!.previousElementSibling!;
+                    if (i === 1) {
+                      el.scrollTop = 0;
+                    } else if (i === number) {
+                      el.scrollTop = el.scrollHeight - el.clientHeight;
+                    } else {
+                      el.scrollTop = ((el.scrollHeight - el.clientHeight) / 3) * (i - 1) + 1;
+                    }
+                  }}
+                ></button>,
+              );
+            return item;
+          })()}
+        </div>
+      </>
+    ) : (
+      <div className={styles.scroller}>{children}</div>
     );
   }
 }
