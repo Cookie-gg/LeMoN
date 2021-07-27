6661c8d065a6ce6d4ccc90c0eea5fdf37f01d475
Cookie <taruta1110@yahoo.co.jp>
2021-07-28 03:46:45 +0900

    add EventTypes

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -4,7 +4,8 @@ import React from 'react';
 declare global {
   type InputTypes = React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
   type FormTypes = React.FormEvent;
+  type EventTypes = React.UIEvent<HTMLDivElement, globalThis.UIEvent>;
   type MountedType = {
-    isMounted: boolean
+    isMounted: boolean;
   };
 }
