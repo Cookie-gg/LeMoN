140c19a3bb76e160eb7ca2909e961efc789adb79
Cookie <taruta1110@yahoo.co.jp>
2021-07-27 23:32:13 +0900

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,14 @@
 import pages from '../assets/scss/pages/About.module.scss';
 import { Frame } from 'components';
+import { useState } from 'react';
 export default function About() {
+  const [section, _section] = useState<number>(1);
+  console.log(section);
   return (
-    <Frame type="scroll">
-      <h1>About Page</h1>
+    <Frame type="scroll" number={2} section={section} _section={(n: number) => _section(n)}>
+      <>
+        <h1>About Page</h1>
+      </>
     </Frame>
   );
 }
