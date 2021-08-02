b7044a24909ea0ddd304edd7bee5e545165a2217
Cookie <taruta1110@yahoo.co.jp>
2021-08-03 08:35:33 +0900

    Update About page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,53 +1,55 @@
 import pages from '../assets/scss/pages/About.module.scss';
-import { PageFrame } from 'components';
-import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
+import { Heading, PageFrame, Svg, Title, List } from 'components';
 import {
   faCalendarAlt,
   faAddressCard,
   faEnvelope,
   faHome,
-  IconDefinition,
 } from '@fortawesome/free-solid-svg-icons';
+import Paragraph from 'components/Paragraph';
 
 export default function About() {
-  const profile = [faCalendarAlt, faAddressCard, faEnvelope, faHome];
-  const profile_content = [
-    '11th November, 2001',
-    'Front-end engineer',
-    'cookie.nkz@gmail.com',
-    'Kanagawa / Japan',
+  const icon = [faCalendarAlt, faAddressCard, faEnvelope, faHome];
+  // from data-base
+  const info = [
+    { content: '11th November, 2001' },
+    { content: 'Front-end engineer' },
+    { content: 'cookie.nkz@gmail.com' },
+    { content: 'Kanagawa / Japan' },
+  ];
+  const career = [
+    { date: new Date('2017/4'), content: 'Entered High School (general course)' },
+    { date: new Date('2020/3'), content: 'Graduated High School' },
+    {
+      date: new Date('2020/4'),
+      content: 'Entered University (information science and engineering)',
+    },
+    { date: new Date('2020/11'), content: 'Joined the intership as a web engineer' },
+    { date: new Date('2021/6'), content: 'Quit the intership' },
   ];
   return (
-    <PageFrame pageStyles={[pages.profile, pages.skills]}>
+    <PageFrame secStyles={[pages.profile, pages.career, pages.skills]} active={pages.active}>
       <>
         {
           <>
-            <h1>PROFILE</h1>
-            <div className={pages.image_wrapper}>
-              <div className={pages.image}></div>
-            </div>
             <div className={pages.text_wrapper}>
-              <h2>
-                <span>Cookie_gg</span>
-              </h2>
-              <ul>
-                {profile.map((el: IconDefinition, i: number) => (
-                  <li key={i}>
-                    {i === 2 ? (
-                      <a href={`mailto:${profile_content[i]}`}>
-                        <Icon icon={el}></Icon>
-                        <p>{profile_content[i]}</p>
-                      </a>
-                    ) : (
-                      <>
-                        <Icon icon={el}></Icon>
-                        <p>{profile_content[i]}</p>
-                      </>
-                    )}
-                  </li>
-                ))}
-              </ul>
+              <Heading rank={1} text="PROFILE" className={pages.heading} />
+              <Title rank={2} text="Cookie_gg" className={pages.title} />
+              <Paragraph
+                className={pages.sentence}
+                text={
+                  '" AIM FOR INTERESTING DEVELOPS "\nI like interesting develops. Any requirement will be interesting depending on the development method. In my opinion, interesting products are born from interesting development.'
+                }
+              />
+              <List list={info} icon={icon} className={pages.info} />
             </div>
+            <Svg name="profile" className={pages.svg} />
+          </>
+        }
+        {
+          <>
+            <Svg name="career" className={pages.svg} />
+            <div className={pages.text_wrapper}></div>
           </>
         }
         {<></>}
