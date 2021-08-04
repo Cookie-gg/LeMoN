fcf1f541db141c9f1a19e836c665b3e52e24013e
Cookie <taruta1110@yahoo.co.jp>
2021-08-04 19:08:47 +0900

    Update component entry point

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,7 +2,7 @@ export { default as Header } from './Header';
 export { default as PageTransition } from './PageTransition';
 export { default as MainFrame } from './MainFrame';
 export { default as PageFrame } from './PageFrame';
-export { default as Svg } from './Svg';
+export { default as SvgFrame } from './SvgFrame';
 export { default as Title } from './Title';
 export { default as Heading } from './Heading';
 export { default as List } from './List';
