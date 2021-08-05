4bcce4dcc17042f8d7a744db1b01ef1f4d4a73de
Cookie <taruta1110@yahoo.co.jp>
2021-08-05 18:11:35 +0900

    delete unnecesary function

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,9 +1,5 @@
 import { SetStateAction } from 'react';
 
-export function formReducer<T>(state: T, e: InputTypes) {
-  const { name, value } = e.target;
-  return { ...state, ...{ [name]: value } };
-}
 export function YMD(targetDate: Date) {
   const date = new Date(targetDate);
   return `${date.getFullYear()}/${date.getMonth()}/${date.getDate()}`;
@@ -11,4 +7,26 @@ export function YMD(targetDate: Date) {
 export function setActiveTime(func: (value: SetStateAction<boolean>) => void, duration: number) {
   func((value) => !value);
   setTimeout(() => func((value) => !value), duration);
+}
+export function sortByDate<T extends { year: number; month: number }[]>(
+  data: T,
+  type: 'asc' | 'desc',
+): T {
+  for (let i = 0; i < data.length - 1; i++) {
+    for (let j = 1; j < data.length - i; j++) {
+      if (
+        type === 'desc'
+          ? data[j - 1].year < data[j].year ||
+            (data[j - 1].year === data[j].year && data[j - 1].month < data[j].month)
+          : data[j - 1].year < data[j].year ||
+            (data[j - 1].year === data[j].year && data[j - 1].month > data[j].month)
+      ) {
+        let tmp: typeof data[0] = { year: 0, month: 0 };
+        tmp = data[j - 1];
+        data[j - 1] = data[j];
+        data[j] = tmp;
+      }
+    }
+  }
+  return data;
 }
