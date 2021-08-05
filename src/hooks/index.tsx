8a61d93046ddf164116a088ea07e4d53225a29a8
Cookie <taruta1110@yahoo.co.jp>
2021-08-05 18:10:35 +0900

    delete unnecesary custom hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -2,5 +2,4 @@ export { default as useMount } from './useMount';
 export { default as useAgent } from './useAgent';
 export { default as usePeriod } from './usePeriod';
 export { default as useFirst } from './useFirst';
-export { default as useNode } from './useNode';
 export { default as useWindowDimensions } from './useWindowDimensions';
