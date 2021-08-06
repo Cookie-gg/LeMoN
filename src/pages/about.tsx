07baa2c9983eb8db7d927e6245ac373233f479ba
Cookie <taruta1110@yahoo.co.jp>
2021-08-07 00:12:39 +0900

    update skills section

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,6 +7,7 @@ import {
   faHome,
 } from '@fortawesome/free-solid-svg-icons';
 import { Cookie_gg, Feelingproud } from 'svg';
+import { Icon as Iconify } from '@iconify/react';
 
 export default function About() {
   const icon = [faCalendarAlt, faAddressCard, faEnvelope, faHome];
@@ -16,6 +17,25 @@ export default function About() {
     { content: 'Front-end engineer' },
     { content: 'cookie.nkz@gmail.com' },
     { content: 'Kanagawa / Japan' },
+  ];
+  const skills: { name: string; icon: string; vscodeIcon: boolean }[] = [
+    { name: 'HTML Living Standard', icon: 'html', vscodeIcon: true },
+    { name: 'CSS3', icon: 'css-3', vscodeIcon: true },
+    { name: 'JavaScript', icon: 'javascript', vscodeIcon: false },
+    { name: 'TypeScript', icon: 'typescript-icon', vscodeIcon: false },
+    { name: 'Webpack', icon: 'webpack', vscodeIcon: false },
+    { name: 'React', icon: 'react', vscodeIcon: false },
+    { name: 'Next.js', icon: 'nextjs', vscodeIcon: false },
+    { name: 'Vue.js', icon: 'vue', vscodeIcon: false },
+    { name: 'Nuxtjs', icon: 'nuxt-icon', vscodeIcon: false },
+    { name: 'Node.js', icon: 'nodejs-icon', vscodeIcon: false },
+    { name: 'Express.js', icon: 'express', vscodeIcon: false },
+    { name: 'NestJS', icon: 'nestjs', vscodeIcon: false },
+    { name: 'MongoDB', icon: 'mongo', vscodeIcon: true },
+    { name: 'GraphQL', icon: 'graphql', vscodeIcon: false },
+    { name: 'GitHub', icon: 'github-icon', vscodeIcon: false },
+    { name: 'C++', icon: 'c-plusplus', vscodeIcon: false },
+    { name: 'C', icon: 'c', vscodeIcon: false },
   ];
   return (
     <PageFrame secStyles={[pages.profile, pages.skills]} active={pages.active}>
@@ -43,6 +63,12 @@ export default function About() {
         {
           <>
             <Heading rank={1} text="SKILLS" className={pages.heading} />
+            <Iconify icon="vscode-icons:file-type-html" />
+            {skills.map((el: { name: string; icon: string; vscodeIcon: boolean }, i: number) => (
+              <li key={i}>
+                <Iconify icon={'logos:' + icon} />
+              </li>
+            ))}
           </>
         }
       </>
--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,5 +1,3 @@
-import Lottie from 'react-lottie-player';
-import * as animationData from '../assets/img/profile.json';
 export default function Works() {
-  return <Lottie loop={false} animationData={animationData} play />;
+  return <div></div>;
 }
