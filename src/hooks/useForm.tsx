83cb117daa56649b42659b146e7a6bfbb2e87770
Cookie <taruta1110@yahoo.co.jp>
2021-08-14 07:56:27 +0900

    update useForm hook

--- src/hooks/useForm.tsx
+++ src/hooks/useForm.tsx
@@ -1,16 +1,23 @@
-import { Dispatch, useReducer } from 'react';
-function useForm<T extends { fileList?: FileList | null }>(
+import { useReducer } from 'react';
+import { encodeImg } from 'utils/common';
+
+function useForm<T>( // 呼び出し側の型に依存
   initialState: T,
-): [T, Dispatch<React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>>] {
-  function reducer(state: T, e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): T {
-    if ((e.target as HTMLInputElement).files) {
-      return { ...state, fileList: (e.target as HTMLInputElement).files };
-    } else {
-      const { name, value } = e.target;
-      return { ...state, ...{ [name]: value } };
-    }
+): [T, (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => Promise<void>] {
+  function reducer(currents: T, updates: { name: string; value: string }): T {
+    return { ...currents, ...{ [updates.name]: updates.value } };
   }
   const [state, dispatch] = useReducer(reducer, initialState);
-  return [state, dispatch];
+  async function asyncReducer(e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): Promise<void> {
+    if ((e.target as HTMLInputElement).files) {
+      const { name, files } = e.target as HTMLInputElement;
+      const encodedFile = await encodeImg(files![0]);
+      dispatch({ name: name, value: encodedFile as string });
+    } else {
+      const { name, value } = e.target;
+      dispatch({ name, value });
+    }
+  }
+  return [state, asyncReducer];
 }
 export default useForm;
