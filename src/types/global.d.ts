2cde8de9b56b2976a975c6e7107afe7eb49659fc
Cookie <taruta1110@yahoo.co.jp>
2021-08-14 07:55:13 +0900

    update global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,7 +2,5 @@ import React from 'react';
 
 // グローバルの型定義
 declare global {
-  // type InputTypes = React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
   type FormTypes = React.FormEvent;
-  // type ScrollTypes = React.UIEvent<HTMLDivElement, globalThis.UIEvent>;
 }
