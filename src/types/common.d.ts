2a901ec2aa839bd2c021662cd72969aba1b2ec5b
Cookie <taruta1110@yahoo.co.jp>
2021-08-18 03:55:47 +0900

    add ZennTypes

--- src/types/common.d.ts
+++ src/types/common.d.ts
@@ -8,3 +8,12 @@ interface PostCMS {
   post: string;
 }
 export type CMSTypes<T> = T extends 'get' ? GetCMS : PostCMS;
+
+export interface ZennTypes {
+  id: string;
+  releaseDate: Date;
+  title: string;
+  emoji: string;
+  type: string;
+  topics: string[];
+}
