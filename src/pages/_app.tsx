61f667a65c217b53310913ec9f8d81ae012d5ca9
Cookie <taruta1110@yahoo.co.jp>
2021-09-09 06:32:15 +0900

    updated _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,27 +5,30 @@ import type { AppProps } from 'next/app';
 import 'assets/scss/foundation/base.scss';
 import 'assets/scss/foundation/reset.scss';
 import 'assets/scss/foundation/global.scss';
-import { Header, MainFrame } from 'components';
+import { Header, MainFrame, ProgressBar } from 'components';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
-const client = new ApolloClient({
+export const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
+    <>
       <Head>
         <meta name="description" content="Cookie's Portfolio" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
-      <Header />
-      <MainFrame>
-        <Component {...pageProps} />
-      </MainFrame>
-    </ApolloProvider>
+      <ProgressBar />
+      <ApolloProvider client={client}>
+        <Header />
+        <MainFrame>
+          <Component {...pageProps} />
+        </MainFrame>
+      </ApolloProvider>
+    </>
   );
 }
 export default MyApp;
