77b4fe52fca0fda7390aae303e6905486f5b46dd
Cookie <taruta1110@yahoo.co.jp>
2021-09-09 06:21:04 +0900

    updated article body component

--- src/components/ArticleBody.tsx
+++ src/components/ArticleBody.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'utils/next';
-import styles from '../assets/scss/components/Markdown.module.scss';
+import styles from '../assets/scss/components/ArticleBody.module.scss';
 import { useEffect, useRef, Dispatch, SetStateAction } from 'react';
 
 interface PropsType {
@@ -12,9 +12,9 @@ export default function ArticleBody({ body, _table }: PropsType) {
   const ref = useRef<HTMLDivElement>(null);
   useEffect(() => {
     const el = ref.current as HTMLDivElement;
-    const resizeObserver = new ResizeObserver((callback) => {
+    const getToc = () => {
       _table(
-        Array.from(callback[0].target.querySelectorAll('h1, h2')).map((el) => {
+        Array.from(el.querySelectorAll('h1, h2')).map((el) => {
           return {
             tagName: el.tagName,
             text: el.textContent as string,
@@ -22,9 +22,16 @@ export default function ArticleBody({ body, _table }: PropsType) {
           };
         }),
       );
-    });
+    };
+    getToc();
+    const resizeObserver = new ResizeObserver(() => getToc());
     resizeObserver.observe(el);
     el.addEventListener('transitionstart', () => resizeObserver.disconnect());
+    el.addEventListener('transitionrun', () => getToc());
+    return () => {
+      el.removeEventListener('transitionstart', () => resizeObserver.disconnect());
+      el.removeEventListener('transitionrun', () => getToc());
+    }
   }, [router.asPath, _table]);
 
   return <div className={styles.body} dangerouslySetInnerHTML={{ __html: body }} ref={ref} />;
