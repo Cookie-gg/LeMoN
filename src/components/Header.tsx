b86cc8695e04dad737e9ff817559f4096b8e1cbb
Cookie <taruta1110@yahoo.co.jp>
2021-09-09 06:25:38 +0900

    updated header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,43 +4,39 @@ import { useRouter } from 'utils/next';
 import { Icon as Iconify } from '@iconify/react';
 import { useAgent, useFirstMount, usePeriod } from 'hooks';
 import styles from '../assets/scss/components/Header.module.scss';
-
-const data = [
-  { name: 'Home', path: '/', icon: 'fa-solid:home' },
-  { name: 'About', path: '/about', icon: 'fa-solid:user' },
-  { name: 'Works', path: '/works', icon: 'fa-solid:code' },
-  { name: 'Blog', path: '/blog', icon: 'fa-solid:newspaper' },
-  { name: 'Contact', path: '/contact', icon: 'fa-solid:envelope' },
-];
+import { useHeaderQuery } from 'types/graphql.d';
 
 export default function Header() {
-  const isMobile = useAgent();
-  const isMounted = useFirstMount();
   const router = useRouter();
-  const [isClicked, _isClicked] = useState<boolean>(false);
+  const isMobile = useAgent();
+  const { data } = useHeaderQuery();
+  const isMounted = useFirstMount();
   const [closing, _closing] = usePeriod(false);
+  const [isClicked, _isClicked] = useState<boolean>(false);
   function clickEvent() {
     _closing(!isMobile ? 950 : 1250);
     _isClicked((prev) => !prev);
   }
-  const list = data.map((el: { name: string; path: string; icon: string }, i: number) => (
-    <li
-      key={i}
-      className={`${router.pathname === el.path && styles.active} ${
-        router.pathname.includes(`${el.path}/`) && styles.lower_active
-      }`}
-      onClick={() => {
-        if (isMobile) clickEvent();
-      }}
-    >
-      <Nlink href={el.path}>
-        <>
-          <Iconify className="sp" icon={el.icon} />
-          <span>{el.name}</span>
-        </>
-      </Nlink>
-    </li>
-  ));
+  const list =
+    data &&
+    data.paths.sortObj<number>('order').map((el: { name: string; path: string; icon: string }, i: number) => (
+      <li
+        key={i}
+        className={`${router.pathname === el.path && styles.active} ${
+          router.pathname.includes(`${el.path}/`) && styles.lower_active
+        }`}
+        onClick={() => {
+          if (isMobile) clickEvent();
+        }}
+      >
+        <Nlink href={el.path}>
+          <>
+            <Iconify className="sp" icon={el.icon} />
+            <span>{el.name}</span>
+          </>
+        </Nlink>
+      </li>
+    ));
   return (
     <header
       className={`${styles.entire} ${isMounted && styles.mounted} ${closing && styles.closing} ${
