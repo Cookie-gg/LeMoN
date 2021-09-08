7f00dd2f51f476cc12321df30d2e8f61a32013a1
Cookie <taruta1110@yahoo.co.jp>
2021-09-09 06:29:28 +0900

    updated useTop hook

--- src/hooks/useTop.tsx
+++ src/hooks/useTop.tsx
@@ -5,9 +5,10 @@ export default function useTop<T extends HTMLElement>(init = 0): [number, RefObj
   const ref = useRef<T>(null);
   const asPath = useRouter().asPath;
   const [top, _top] = useState(init);
-  
+
   useEffect(() => {
     const el = ref.current as T;
+    _top(el.getBoundingClientRect().top);
     const resizeObserver = new ResizeObserver((callback) => _top(callback[0].target.getBoundingClientRect().top));
     resizeObserver.observe(el);
     el.addEventListener('transitionstart', () => resizeObserver.disconnect());
