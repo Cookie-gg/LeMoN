2aecc66a9e5c6ed6cb6ee26580aa1b18ea1b2577
Cookie <taruta1110@yahoo.co.jp>
2021-09-09 06:23:22 +0900

    updated name plate component

--- src/components/NamePlate.tsx
+++ src/components/NamePlate.tsx
@@ -1,13 +1,11 @@
-import { memo } from 'react';
 import { Cookie_gg } from 'svg';
-import { compare } from 'utils/common';
 import styles from '../assets/scss/components/NamePlate.module.scss';
 
 interface PropsType {
   className?: string;
 }
 
-function NamePlate({ className }: PropsType) {
+export default function NamePlate({ className }: PropsType) {
   return (
     <h2 className={`${className} ${styles.entire}`}>
       <span className={styles.svg}>
@@ -17,5 +15,3 @@ function NamePlate({ className }: PropsType) {
     </h2>
   );
 }
-
-export default memo(NamePlate, (prev: PropsType, next: PropsType) => compare<PropsType>(prev, next));
