f8b5bdcb7a51b4c94dfb62358adc2285f2d0ac40
Cookie <taruta1110@yahoo.co.jp>
2021-09-16 10:25:34 +0900

    update env files

--- src/graphql/config.gql.ts
+++ src/graphql/config.gql.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://lemon-backend.an.r.appspot.com/graphql',
+  uri: process.env.NEXT_PUBLIC_BACKEND_API_URL,
   cache: new InMemoryCache(),
 });
