b3410f6b2a7663166456032c54ec0ee0f12211dc
Cookie <taruta1110@yahoo.co.jp>
2021-09-19 21:33:00 +0900

    update nextjs modules

--- src/utils/next.ts
+++ src/utils/next.ts
@@ -4,10 +4,5 @@ export { default as Image } from 'next/image';
 export { default as Link } from 'next/link';
 export { default as Script } from 'next/script';
 export { useRouter } from 'next/router';
-export type {
-  GetStaticProps,
-  GetServerSideProps,
-  GetStaticPaths,
-  NextApiRequest,
-  NextApiResponse,
-} from 'next';
+export { Router } from 'next/router';
+export type { GetStaticProps, GetServerSideProps, GetStaticPaths, NextApiRequest, NextApiResponse } from 'next';
