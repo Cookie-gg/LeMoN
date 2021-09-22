649d05fae261ed6c386aabf5c2367b65a71f6670
Cookie <taruta1110@yahoo.co.jp>
2021-09-22 10:22:12 +0900

    test og-image

--- src/components/HeadMeta.tsx
+++ src/components/HeadMeta.tsx
@@ -4,10 +4,11 @@ import { Head, useRouter } from 'utils/next';
 interface PropsType {
   title: string;
   description?: string;
+  ogImage?: string;
   children?: ReactElement;
 }
 
-export default function HeadMeta({ title, description, children }: PropsType) {
+export default function HeadMeta({ title, description, ogImage, children }: PropsType) {
   const asPath = useRouter().asPath;
   return (
     <Head>
@@ -18,7 +19,7 @@ export default function HeadMeta({ title, description, children }: PropsType) {
       <meta property="og:site_name" content="LeMoN" />
       <meta property="og:description" content={description ? description : `${title} page of Cookie_gg's Portfolio`} />
       <meta property="og:type" content="website" />
-      {/* <meta property="og:image" content={imgUrl} /> */}
+      <meta property="og:image" content={ogImage} />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="630" />
       <meta name="twitter:card" content="summary_large_image" />
