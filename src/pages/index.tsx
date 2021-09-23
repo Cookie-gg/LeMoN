0990969cef78578aef8635ddfea649a63b663a46
Cookie <taruta1110@yahoo.co.jp>
2021-09-23 15:10:15 +0900

    performance test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { memo } from 'react';
 import { HeadMeta } from 'components';
-import { specifor } from 'utils/common';
 import pages from '../assets/scss/pages/Home.module.scss';
 import { useFirstMount } from 'hooks';
 
@@ -14,14 +13,6 @@ function Home() {
           <span className={pages.text}>LeMoN</span>
         </span>
       </h1>
-      <div className={`${pages.phrase} exclude`}>
-        {specifor(10, (i: number) => (
-          <div key={i}>
-            <span className="pc">WEB DEVELOP</span>
-            <span className="sp">WEB DEV</span>
-          </div>
-        ))}
-      </div>
     </>
   );
 }
