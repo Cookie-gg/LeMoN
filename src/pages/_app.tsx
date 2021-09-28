cc4927288016497ca6b72dacf8b439b3f0b55799
Cookie <taruta1110@yahoo.co.jp>
2021-09-28 18:29:57 +0900

    import performances

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,9 @@
 import 'utils/prototype';
-import { dynamic } from 'utils/next';
 import type { AppProps } from 'next/app';
 import 'assets/scss/foundations/base.scss';
 import 'assets/scss/foundations/reset.scss';
 import 'assets/scss/foundations/global.scss';
-import { MainFrame, ProgressBar } from 'components';
-
-const Header = dynamic(() => import('components/Header'), { ssr: false });
+import { Header, MainFrame, ProgressBar } from 'components';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
