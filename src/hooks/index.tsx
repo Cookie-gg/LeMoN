8cff4ed386fe8408ac86787999d5ef7ca96f273b
Cookie <taruta1110@yahoo.co.jp>
2021-10-20 23:04:25 +0900

    delete unnecessary modules

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,5 +1,6 @@
 export { default as useTop } from './useTop';
 export { default as useForm } from './useForm';
+export { default as useAuth } from './useAuth';
 export { default as useMount } from './useMount';
 export { default as useAgent } from './useAgent';
 export { default as usePeriod } from './usePeriod';
@@ -8,7 +9,4 @@ export { default as useBrowser } from './useBrowser';
 export { default as usePrevious } from './usePrevious';
 export { default as useIntersect } from './useIntersect';
 export { default as useFirstPeriod } from './useFirstPeriod';
-export { default as useAuth } from './useAuth';
 export { default as useWindowDimensions } from './useWindowDimensions';
-
-export { default as useContact } from './useContact';
