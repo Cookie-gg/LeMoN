8ca4dc1d1196f80a96cbf25db88438697d1c5b1b
Cookie <taruta1110@yahoo.co.jp>
2021-12-16 11:26:54 +0900

    refactoring

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -18,22 +18,15 @@ export const encodeEmoji: (emoji: string) => string = (emoji) => {
   return comp.toString(16);
 };
 
-export const displayDate = (date: Date, split = '/', compare = true) => {
+export const displayDate = (arg: Date, split = '/', compare = true) => {
+  const date = { y: arg.getFullYear(), m: arg.getMonth(), d: arg.getDate() };
+  const display = `${date.y + split}${`0${date.m - 1}`.slice(-2)}${split}${`0${date.d}`.slice(-2)}`;
   if (compare) {
     const now = new Date();
-    if (
-      now.getFullYear().toString() === date.getFullYear().toString() &&
-      now.getMonth().toString() === date.getMonth().toString()
-    ) {
-      return `${now.getDate() - date.getMonth()} days ago`;
-    } else
-      return `${date.getFullYear()}${split}${`0${date.getMonth() - 1}`.slice(-2)}${split}${`0${date.getDate()}`.slice(
-        -2,
-      )}`;
-  } else
-    return `${date.getFullYear()}${split}${`0${date.getMonth() - 1}`.slice(-2)}${split}${`0${date.getDate()}`.slice(
-      -2,
-    )}`;
+    if (now.getFullYear() === date.y && now.getMonth() === date.m && now.getDate() > date.d)
+      return `${now.getDate() - date.d} days ago`;
+    else return display;
+  } else return display;
 };
 
 export const specifor: <T>(times: number, func: (index: number) => T) => T[] = (times, func) => {
@@ -44,5 +37,3 @@ export const specifor: <T>(times: number, func: (index: number) => T) => T[] = (
   return list;
 };
 
-export const EmojiValidation =
-  /^(?:(?!(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff]))[^`~!+,!@#$%^&*();\/|<>"0-9=[\]{}_☺]){1,255}$/;
