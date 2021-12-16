83c55314a7ca8cc271cb83f5fd1122df3e375762
Cookie <taruta1110@yahoo.co.jp>
2021-12-16 11:28:00 +0900

    added page 'edit/index'

---
+++ src/pages/edit/index.tsx
@@ -0,0 +1,43 @@
+import { memo } from 'react';
+import { GetStaticProps } from 'utils/next';
+import editQuery, { EditQueryType } from 'data/editQuery';
+import styles from '../../assets/scss/pages/Edit.module.scss';
+import { Heading, PageFrame, ArticleList, HeadMeta } from 'components';
+
+function Page({ data }: { data: EditQueryType }) {
+  data = JSON.parse(String(data));
+  return (
+    <>
+      <HeadMeta title="Edit" ogImage={`${process.env.NEXT_PUBLIC_OG_IMAGE}/page/edit`} />
+      <PageFrame classNmae={styles.page}>
+        <>
+          <Heading className={styles.heading} rank={1} text="PRIVATE" />
+          <ArticleList
+            className={styles.articles}
+            data={data.all.articles}
+            display={data.all.articles.length}
+            vertical
+            editable
+            type="private"
+          />
+          <Heading className={styles.heading} rank={1} text="PUBLIC" />
+          <ArticleList
+            className={styles.articles}
+            data={data.all.articles}
+            display={data.all.articles.length}
+            vertical
+            editable
+            type="public"
+          />
+        </>
+      </PageFrame>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => ({
+  props: { data: JSON.stringify(await editQuery()) },
+  revalidate: 60,
+});
+
+export default memo(Page);
