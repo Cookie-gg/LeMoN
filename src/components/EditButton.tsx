fb9b5c10316e7fe5fbaaab2f7807b8f80ae3029d
Cookie <taruta1110@yahoo.co.jp>
2021-12-16 11:44:13 +0900

    componetization

---
+++ src/components/EditButton.tsx
@@ -0,0 +1,28 @@
+import { memo } from 'react';
+import { Link } from 'utils/next';
+import { Icon as Iconify } from '@iconify/react';
+import styles from '../assets/scss/components/EditButton.module.scss';
+
+function EditButton({ articleId }: { articleId: string }) {
+  return (
+    <div className={styles.entire}>
+      <Link href={`/edit/${articleId}`}>
+        <a>
+          <button className={styles.editor}>
+            <Iconify icon="ic:round-edit-note" />
+          </button>
+        </a>
+      </Link>
+      <Link href={`https://github.com/Cookie-gg/zenn-content/blob/master/articles/${articleId}.md`}>
+        <a target="_blank">
+          <button className={styles.github}>
+            <Iconify icon="ic:round-open-in-new" />
+            Open in GitHub
+          </button>
+        </a>
+      </Link>
+    </div>
+  );
+}
+
+export default memo(EditButton);
--- src/pages/blog/[...id].tsx
+++ src/pages/blog/[...id].tsx
@@ -3,100 +3,73 @@ import articleQuery from 'data/articleQuery';
 import { Zenn, ZennAdds } from 'types/common';
 import { memo, useEffect, useRef, useState } from 'react';
 import styles from '../../assets/scss/pages/Blog.module.scss';
-import { GetStaticPaths, GetStaticProps, Link, useRouter } from 'utils/next';
+import { GetStaticPaths, GetStaticProps, useRouter } from 'utils/next';
 import {
   Heading,
   HeadMeta,
   PageFrame,
+  EditButton,
   ArticleToc,
   ArticleBody,
   ArticleMeta,
   ArticleList,
   ArticleTopics,
 } from 'components';
-import { Icon as Iconify } from '@iconify/react';
 
 function Page({ data, auth }: { data: Zenn & ZennAdds; auth: { state: boolean } }) {
+  const router = useRouter();
   data = JSON.parse(String(data));
   const contentsRef = useRef<HTMLDivElement>(null);
   const [activeSection, _activeSection] = useState(0);
-  const [editorState, _editorState] = useState<'visible' | 'hidden' | 'disable'>('disable');
-  const router = useRouter();
   useEffect(() => _activeSection(0), [router.query.id]);
   return (
     <>
       <HeadMeta title={data.title} ogImage={`${process.env.NEXT_PUBLIC_OG_IMAGE}/article/${data.title}`}>
         <link rel="pagesheet" href="https://cdn.jsdelivr.net/npm/katex@0.13.13/dist/katex.min.css" />
       </HeadMeta>
-      <PageFrame classNmae={`${styles.post} ${editorState === 'visible' && styles.visible}`}>
+      <PageFrame classNmae={styles.post}>
         <>
-          <>
-            <ArticleMeta
-              emoji={data.emoji}
-              title={data.title}
-              published={data.published}
-              releaseDate={data.releaseDate}
-              updateDate={data.updateDate}
-            />
-            <ArticleTopics
-              type={data.type}
-              topics={data.topics}
-              icons={data.icons}
-              className={styles.topics}
-              inArticle
-            />
-            <div className={styles.contents} ref={contentsRef}>
-              <main>
-                <ArticleBody
-                  html={data.html}
-                  headingTexts={data.headings ? data.headings.map((heading) => heading.text) : undefined}
-                  _activeSection={(n: number) => _activeSection(n)}
-                >
-                  <ArticleToc
-                    meta={{ title: data.title, emoji: data.emoji }}
-                    activeSection={activeSection}
-                    headings={data.headings}
-                  />
-                </ArticleBody>
-                <Heading rank={2} text={post.relations.title} className={styles.heading} />
-                <ArticleList
-                  vertical
-                  className={styles.relations}
-                  data={data.relations.articles}
-                  display={data.relations.articles.length}
-                  needDateParse
-                />
-              </main>
-              <aside>
-                {auth.state && (
-                  <div className={styles.edit}>
-                    <Link href={`/edit/${data.articleId}`}>
-                      <a>
-                        <button onClick={() => _editorState('visible')} className={styles.editor}>
-                          <Iconify icon="ic:round-edit-note" />
-                        </button>
-                      </a>
-                    </Link>
-                    <Link href={`https://github.com/Cookie-gg/zenn-content/blob/master/articles/${data.articleId}.md`}>
-                      <a target="_blank">
-                        <button className={styles.github}>
-                          <Iconify icon="ic:round-open-in-new" />
-                          Open in GitHub
-                        </button>
-                      </a>
-                    </Link>
-                  </div>
-                )}
-                <ArticleTopics type={data.type} topics={data.topics} icons={data.icons} inArticle />
+          <ArticleMeta
+            emoji={data.emoji}
+            title={data.title}
+            published={data.published}
+            releaseDate={data.releaseDate}
+            updateDate={data.updateDate}
+          />
+          <ArticleTopics type={data.type} topics={data.topics} icons={data.icons} className={styles.topics} inArticle />
+          <div className={styles.contents} ref={contentsRef}>
+            <main>
+              <ArticleBody
+                html={data.html}
+                headingTexts={data.headings ? data.headings.map((heading) => heading.text) : undefined}
+                _activeSection={(n: number) => _activeSection(n)}
+              >
                 <ArticleToc
                   meta={{ title: data.title, emoji: data.emoji }}
                   activeSection={activeSection}
                   headings={data.headings}
-                  className={styles.toc}
                 />
-              </aside>
-            </div>
-          </>
+              </ArticleBody>
+              <Heading rank={2} text={post.relations.title} className={styles.heading} />
+              <ArticleList
+                vertical
+                className={styles.relations}
+                data={data.relations.articles}
+                display={data.relations.articles.length}
+                needDateParse
+              />
+            </main>
+            <aside>
+              {auth.state && <EditButton articleId={data.articleId} />}
+              <ArticleTopics type={data.type} topics={data.topics} icons={data.icons} inArticle />
+              <ArticleToc
+                meta={{ title: data.title, emoji: data.emoji }}
+                activeSection={activeSection}
+                headings={data.headings}
+                className={styles.toc}
+              />
+            </aside>
+          </div>
         </>
       </PageFrame>
     </>
@@ -104,7 +77,7 @@ function Page({ data, auth }: { data: Zenn & ZennAdds; auth: { state: boolean } 
 }
 
 export const getStaticPaths: GetStaticPaths = async () => ({
-  paths: (await articleQuery()).id.map((obj) => ({ params: { id: obj.id } })),
+  paths: (await articleQuery()).paths,
   fallback: false,
 });
 
