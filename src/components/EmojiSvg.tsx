5c261f9f7795c88fbdd8dd42469cfc0eb22bf655
Cookie <taruta1110@yahoo.co.jp>
2021-12-30 16:51:52 +0900

    support className as a prop

--- src/components/EmojiSvg.tsx
+++ src/components/EmojiSvg.tsx
@@ -1,10 +1,21 @@
 import { memo } from 'react';
+import styles from '../assets/scss/components/EmojiSvg.module.scss';
 
-function EmojiSvg({ width = 22.5, height = 22.5, unicode }: { width?: number; height?: number; unicode: string }) {
+function EmojiSvg({
+  width = 22.5,
+  height = 22.5,
+  unicode,
+  className,
+}: {
+  width?: number;
+  height?: number;
+  unicode: string;
+  className?: string;
+}) {
   function createSvg(_unicode: string) {
     return (
-      <svg width={`${width}`} height={`${height}`}>
-        <use href={`/twiEmoji.svg#${_unicode}`} />
+      <svg width={`${width}`} height={`${height}`} className={`${styles.entire} ${className}`}>
+        <use href={`/twemoji.svg#${_unicode}`} />
       </svg>
     );
   }
