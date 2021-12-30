263e048001166a558749d8952f700bb447a667bd
Cookie <taruta1110@yahoo.co.jp>
2021-12-30 16:35:36 +0900

    deleted unnecessary methods

--- src/utils/prototype.ts
+++ src/utils/prototype.ts
@@ -8,16 +8,6 @@ Array.prototype.specifor = function (times, func) {
   return list;
 };
 
-Array.prototype.sortObj = function <U>(key: string, type: 'asc' | 'desc' = 'desc', length?: boolean) {
-  return this.slice().sort((a, b) => {
-    const a_target = length ? (a[key] as []).length : (a[key] as U);
-    const b_target = length ? (b[key] as []).length : (b[key] as U);
-    if (a_target < b_target) return type === 'desc' ? 1 : -1;
-    if (a_target > b_target) return type === 'desc' ? -1 : 1;
-    return 0;
-  });
-};
-
 Array.prototype.rangeMap = function (start, end, func) {
   const list = [];
   for (let i = start; i <= end; i++) {
