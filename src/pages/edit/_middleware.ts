d884ec1126a64c987a41b60d1d8e797651d01ed8
Cookie <taruta1110@yahoo.co.jp>
2021-12-30 22:25:03 +0900

    deleted middleware

--- src/pages/edit/_middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export async function middleware(req: NextRequest) {
-  if (req.cookies.token === undefined) return NextResponse.redirect('/login');
-  return NextResponse.next();
-}
