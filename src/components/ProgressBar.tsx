73da3bdd30a9988d4cefd82ad8396c3263665ee3
Cookie <taruta1110@yahoo.co.jp>
2021-12-30 16:42:15 +0900

    improved a progress bar

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -2,26 +2,22 @@ import Nprogress from 'nprogress';
 import { useEffect, useState } from 'react';
 import { Router, useRouter } from 'next/router';
 
-Nprogress.configure({ showSpinner: false, speed: 500, minimum: 0.25 });
 export default function ProgressBar() {
-  Router.events.on('routeChangeStart', () => {
-    Nprogress.start();
-  });
-
-  Router.events.on('routeChangeComplete', () => {
-    Nprogress.done();
-  });
-
-  Router.events.on('routeChangeError', () => {
-    Nprogress.done();
-  });
-
   const router = useRouter();
-  const dependencies = router.pathname === '/blog/[...id]' ? router.query.id : router.asPath;
   const [isMounted, _isMounted] = useState(false);
+  Nprogress.configure({ showSpinner: false, speed: 500, minimum: 0.25 });
+  const dependencies = router.asPath.split('?')[0];
   useEffect(() => {
+    Router.events.on('routeChangeStart', () => Nprogress.start());
+    Router.events.on('routeChangeComplete', () => Nprogress.done());
+    Router.events.on('routeChangeError', () => Nprogress.done());
     setTimeout(() => _isMounted(true), 0);
-    return () => _isMounted(false);
+    return () => {
+      _isMounted(false);
+      Router.events.off('routeChangeStart', () => Nprogress.start());
+      Router.events.off('routeChangeComplete', () => Nprogress.done());
+      Router.events.off('routeChangeError', () => Nprogress.done());
+    };
   }, [dependencies]);
 
   return <noscript className={`${isMounted ? 'mounted' : 'yet'}`}></noscript>;
