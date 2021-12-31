3e10f2123aaea3fbc217dbfaecd00c8caa372a6e
Cookie <taruta1110@yahoo.co.jp>
2021-12-31 14:54:53 +0900

    improved perfomance of mobile

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -1,12 +1,14 @@
 import { memo } from 'react';
+import { useAgent } from 'hooks';
 import { useRouter } from 'utils/next';
 import styles from '../assets/scss/components/PageTransition.module.scss';
 
 function PageTransition() {
+  const isMobile = useAgent('mobile');
   const pathname = useRouter().pathname;
   return (
     <>
-      <div className={`${styles.entire} ${pathname == '/' && styles.home} exclude`}>
+      <div className={`${styles.entire} ${pathname == '/' && styles.home} ${isMobile && styles.mobile} exclude`}>
         <div className={styles.cover}>
           <span>LeMoN</span>
         </div>
