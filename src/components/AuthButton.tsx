5ef572c4a1936e2b0efc5856ab2721e2452085de
Cookie <taruta1110@yahoo.co.jp>
2022-01-01 22:48:58 +0900

    added function to clear scroll top cashe

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -2,6 +2,7 @@ import { memo } from 'react';
 import styles from '../assets/scss/components/AuthButton.module.scss';
 import { Icon as Iconify } from '@iconify/react';
 import { Link, useRouter } from 'utils/next';
+import { Nlink } from 'components';
 
 function AuthButton({ logout }: { logout: () => Promise<void> }) {
   const path = useRouter().asPath;
@@ -10,11 +11,9 @@ function AuthButton({ logout }: { logout: () => Promise<void> }) {
       <a className={styles.log} title="ログアウト" onClick={async () => logout()}>
         <Iconify fr={''} icon="ic:round-logout" />
       </a>
-      <Link href="/edit">
-        <a title="記事一覧">
-          <button />
-        </a>
-      </Link>
+      <Nlink href="/edit" title="記事一覧">
+        <button />
+      </Nlink>
       <Link href="/edit/no-title">
         <a className={`${styles.article} ${path.split('/')[2] === 'no-title' && styles.active}`} title="記事新規作成">
           <Iconify fr={''} icon="ic:round-mode-edit-outline" />
