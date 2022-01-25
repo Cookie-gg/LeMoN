85346f20285cb9ef95fa7f81324ea376d59ebd7c
Cookie <taruta1110@yahoo.co.jp>
2022-01-25 22:33:47 +0900

    changed a place ApolloProvider

--- src/components/PageFrame.tsx
+++ src/components/PageFrame.tsx
@@ -1,8 +1,14 @@
 import { memo, ReactElement } from 'react';
+import { client } from 'graphql/config.gql';
+import { ApolloProvider } from '@apollo/client';
 import styles from '../assets/scss/components/PageFrame.module.scss';
 
 function PageFrame({ children, className }: { children: ReactElement; className?: string }) {
-  return <div className={`${styles.entire} ${className}`}>{children}</div>;
+  return (
+    <ApolloProvider {...{ client }}>
+      <div className={`${styles.entire} ${className}`}>{children}</div>
+    </ApolloProvider>
+  );
 }
 
 export default memo(PageFrame);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,21 +5,16 @@ import 'assets/scss/foundations/base.scss';
 import 'assets/scss/foundations/reset.scss';
 import 'assets/scss/foundations/global.scss';
 import { Header, MainFrame, ProgressBar } from 'components';
-import { client } from 'graphql/config.gql';
-import { ApolloProvider } from '@apollo/client';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [state, login, logout] = useAuth();
-
   return (
     <>
       <ProgressBar />
       <Header />
-      <ApolloProvider {...{ client }}>
-        <MainFrame auth={{ state, logout }}>
-          <Component {...pageProps} auth={{ state, login }} />
-        </MainFrame>
-      </ApolloProvider>
+      <MainFrame auth={{ state, logout }}>
+        <Component {...pageProps} auth={{ state, login }} />
+      </MainFrame>
     </>
   );
 }
