41eebc78428a15907b9b5da628887ba8f00e956d
Cookie <taruta1110@yahoo.co.jp>
2022-01-25 22:41:13 +0900

    Revert "changed a place ApolloProvider"
    This reverts commit 85346f20285cb9ef95fa7f81324ea376d59ebd7c.

--- src/components/PageFrame.tsx
+++ src/components/PageFrame.tsx
@@ -1,14 +1,8 @@
 import { memo, ReactElement } from 'react';
-import { client } from 'graphql/config.gql';
-import { ApolloProvider } from '@apollo/client';
 import styles from '../assets/scss/components/PageFrame.module.scss';
 
 function PageFrame({ children, className }: { children: ReactElement; className?: string }) {
-  return (
-    <ApolloProvider {...{ client }}>
-      <div className={`${styles.entire} ${className}`}>{children}</div>
-    </ApolloProvider>
-  );
+  return <div className={`${styles.entire} ${className}`}>{children}</div>;
 }
 
 export default memo(PageFrame);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,21 @@ import 'assets/scss/foundations/base.scss';
 import 'assets/scss/foundations/reset.scss';
 import 'assets/scss/foundations/global.scss';
 import { Header, MainFrame, ProgressBar } from 'components';
+import { client } from 'graphql/config.gql';
+import { ApolloProvider } from '@apollo/client';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [state, login, logout] = useAuth();
+
   return (
     <>
       <ProgressBar />
       <Header />
-      <MainFrame auth={{ state, logout }}>
-        <Component {...pageProps} auth={{ state, login }} />
-      </MainFrame>
+      <ApolloProvider {...{ client }}>
+        <MainFrame auth={{ state, logout }}>
+          <Component {...pageProps} auth={{ state, login }} />
+        </MainFrame>
+      </ApolloProvider>
     </>
   );
 }
