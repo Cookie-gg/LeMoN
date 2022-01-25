8af4d3c625e72b1a36e81effd3f2a61e109030c3
Cookie <taruta1110@yahoo.co.jp>
2022-01-25 17:23:44 +0900

    refactoring

--- src/pages/edit/[...id].tsx
+++ src/pages/edit/[...id].tsx
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { memo } from 'react';
 import nookies from 'nookies';
 import { client } from 'graphql/config.gql';
@@ -7,17 +6,13 @@ import { Editor, PageFrame } from 'components';
 import { GetServerSideProps, Head } from 'utils/next';
 import styles from '../../assets/scss/pages/Edit.module.scss';
 import { FindArticleDocument, FindArticleQuery } from 'types/graphql.d';
+import { auth, cookie } from 'utils/common';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   try {
-    await axios.get(`${process.env.NEXT_PUBLIC_MELON}/status`, {
-      headers: {
-        key: `${process.env.NEXT_PUBLIC_AUTH_KEY}`,
-        authorization: `bearer ${ctx.req.headers.cookie?.replace('token=', '')}`,
-      },
-    });
+    await auth('get', '/status', cookie(ctx.req.headers.cookie).token);
   } catch {
-    nookies.destroy(ctx, 'token');
+    nookies.destroy(null, 'token');
     return { redirect: { destination: '/login', permanent: false } };
   } finally {
     try {
@@ -35,7 +30,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 function Page({ data }: { data: Partial<Zenn & ZennAdds> }) {
   data = JSON.parse(String(data));
   return (
-    <PageFrame classNmae={styles.post}>
+    <PageFrame className={styles.post}>
       <>
         <Head>
           <title>Edit | LeMoN</title>
--- src/pages/edit/index.tsx
+++ src/pages/edit/index.tsx
@@ -1,22 +1,17 @@
-import axios from 'axios';
 import { memo } from 'react';
 import nookies from 'nookies';
 import { GetServerSideProps } from 'utils/next';
 import editQuery, { EditQueryType } from 'data/editQuery';
 import styles from '../../assets/scss/pages/Edit.module.scss';
 import { Heading, PageFrame, ArticleList, HeadMeta } from 'components';
+import { auth, cookie } from 'utils/common';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   try {
-    await axios.get(`${process.env.NEXT_PUBLIC_MELON}/status`, {
-      headers: {
-        key: `${process.env.NEXT_PUBLIC_AUTH_KEY}`,
-        authorization: `bearer ${ctx.req.headers.cookie?.replace('token=', '')}`,
-      },
-    });
+    await auth('get', '/status', cookie(ctx.req.headers.cookie).token);
     return { props: { data: JSON.stringify(await editQuery()) } };
   } catch {
-    nookies.destroy(ctx, 'token');
+    nookies.destroy(null, 'token');
     return { redirect: { destination: '/login', permanent: false } };
   }
 };
@@ -26,7 +21,7 @@ function Page({ data }: { data: EditQueryType }) {
   return (
     <>
       <HeadMeta title="Edit" ogImage={`${process.env.NEXT_PUBLIC_OG_IMAGE}/page/edit`} />
-      <PageFrame classNmae={styles.page}>
+      <PageFrame className={styles.page}>
         <>
           <Heading className={styles.heading} rank={1} text="PRIVATE" />
           <ArticleList className={styles.articles} data={data.articles} vertical editable type="private" />
