49ffabe68dc1ce302f126ce0bc18b3ef6286b5e7
Cookie <taruta1110@yahoo.co.jp>
2022-01-25 17:23:26 +0900

    refactoring

--- src/pages/edit/no-title.tsx
+++ src/pages/edit/no-title.tsx
@@ -1,28 +1,23 @@
-import axios from 'axios';
 import { memo } from 'react';
 import nookies from 'nookies';
 import { Editor, PageFrame } from 'components';
 import { GetServerSideProps, Head } from 'utils/next';
 import styles from '../../assets/scss/pages/Edit.module.scss';
+import { auth, cookie } from 'utils/common';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   try {
-    await axios.get(`${process.env.NEXT_PUBLIC_MELON}/status`, {
-      headers: {
-        key: `${process.env.NEXT_PUBLIC_AUTH_KEY}`,
-        authorization: `bearer ${ctx.req.headers.cookie?.replace('token=', '')}`,
-      },
-    });
+    await auth('get', '/status', cookie(ctx.req.headers.cookie).token);
     return { props: {} };
   } catch {
-    nookies.destroy(ctx, 'token');
+    nookies.destroy(null, 'token');
     return { redirect: { destination: '/login', permanent: false } };
   }
 };
 
 function Page() {
   return (
-    <PageFrame classNmae={styles.post}>
+    <PageFrame className={styles.post}>
       <>
         <Head>
           <title>Edit | LeMoN</title>
